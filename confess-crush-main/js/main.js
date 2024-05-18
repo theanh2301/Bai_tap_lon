12d53810c11c0d0917bf90757693e32b24a10a1a
theanh2301 <131929305+theanh2301@users.noreply.github.com>
2024-05-18 23:39:46 +0700

    Add files via upload
    update

--- confess-crush-main/js/main.js
+++ confess-crush-main/js/main.js
@@ -35,7 +35,7 @@ function firstQuestion() {
         imageUrl: 'img/logi.gif',
         imageWidth: 300,
         imageHeight: 300,
-        background: '#fff url("img/iput-bg.jpg")',
+        background: ' url("img/iput-bg.jpg")',
         imageAlt: 'Custom image',
         confirmButtonText: CONFIG.btnIntro
     }).then(function() {
